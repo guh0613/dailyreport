f4e565deda5cef2a594eea09cee709a7426db805
guh0613 <gux1129@126.com>
2018-11-03 15:49:33 +0800

    first commit

--- app/src/main/java/com/engu/dailyreport/MainActivity.java
+++ app/src/main/java/com/engu/dailyreport/MainActivity.java
@@ -1,14 +1,22 @@
 package com.engu.dailyreport;
 
-import android.app.*;
 import android.os.*;
+import android.support.v7.app.*;
+import android.support.v7.widget.*;
+import android.support.design.widget.*;
+import android.view.*;
+import android.widget.EditText;
+import android.text.*;
+import android.util.*;
 
-public class MainActivity extends Activity 
+public class MainActivity extends AppCompatActivity 
 {
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+		Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
     }
 }
